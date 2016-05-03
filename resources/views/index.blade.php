7eacc28324e0de84688a8d2cea06c3f80ff23046
Fikri Reza Alhadi <fikrirezaa@gmail.com>
2016-05-03 16:37:28 +0700

    fix login form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -83,11 +83,11 @@
                         <form action="{{ url('login') }}" method="post">
                           {!! csrf_field() !!}
                           <div class="form-group has-feedback">
-                            <input type="email" class="form-control" placeholder="Email">
+                            <input name="email" type="email" class="form-control" placeholder="Email">
                             <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                           </div>
                           <div class="form-group has-feedback">
-                            <input type="password" class="form-control" placeholder="Password">
+                            <input name="password" type="password" class="form-control" placeholder="Password">
                             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                           </div>
                           <div class="row">
