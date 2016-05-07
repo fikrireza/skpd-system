a95e5f749a08d04442d2bf6b7030807e28bcf3a4
dudyali <dudyali@gmail.com>
2016-05-07 17:04:12 +0700

    ganti footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
 <div class="pull-right hidden-xs">
   <b>Version</b> 1.0
 </div>
-<strong>Copyright &copy; 2016-2017 <a href="http://9tins.com">9TINS Lab Projects</a>.</strong> All rights reserved.
+<strong>Copyright &copy; 2016-2017 <a href="http://tangerangkab.go.id">Pemerintah Kabupaten Tangerang</a>.</strong> All rights reserved.
