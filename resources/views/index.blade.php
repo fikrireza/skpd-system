cc0dfdafd7fd5551f42cc8dcb0d9521a4e8abd90
Fikri Reza Alhadi <fikrirezaa@gmail.com>
2016-05-16 14:35:21 +0700

    welcome page edit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -237,12 +237,12 @@
                   <ul class="products-list product-list-in-box">
                     <li class="item">
                       <div class="product-img">
-                        <img src="dist/img/default-50x50.gif">
+                        <img src="dist/img/user4-128x128.jpg">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Samsung TV</a>
+                        <a href="" class="product-title">Amanda Satyarini</a>
                         <span class="product-description">
-                          Samsung 32" 1080p 60Hz LED Smart HDTV.
+                          Yth. SKPD Kabupaten Tangerang terkait, Jika No. KK tidak terbaca pada saat Pendaftaran online bagaimana? Mohon informasinya, terima kasih.
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -251,9 +251,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Bicycle</a>
+                        <a href="" class="product-title">Dudy</a>
                         <span class="product-description">
-                          26" Mongoose Dolomite Men's 7-speed, Navy Blue.
+                          Tentang pembuatan ktp nih....
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -262,9 +262,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Xbox One</a>
+                        <a href="" class="product-title">Bayu</a>
                         <span class="product-description">
-                          Xbox One Console Bundle with Halo Master Chief Collection.
+                          Tolong diinformasikan tentang....
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -295,9 +295,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Samsung TV</a>
+                        <a href="" class="product-title">Contoh</a>
                         <span class="product-description">
-                          Samsung 32" 1080p 60Hz LED Smart HDTV.
+                          Ini hanya sebagai contoh...
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -306,9 +306,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Bicycle</a>
+                        <a href="" class="product-title">Contoh</a>
                         <span class="product-description">
-                          26" Mongoose Dolomite Men's 7-speed, Navy Blue.
+                          Ini hanya sebagai contoh...
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -317,9 +317,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Xbox One</a>
+                        <a href="" class="product-title">Contoh</a>
                         <span class="product-description">
-                          Xbox One Console Bundle with Halo Master Chief Collection.
+                          Ini hanya sebagai contoh...
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -349,9 +349,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Samsung TV</a>
+                        <a href="" class="product-title">Parjo</a>
                         <span class="product-description">
-                          Samsung 32" 1080p 60Hz LED Smart HDTV.
+                          Biaya upah pekerja sini...
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -360,9 +360,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Bicycle</a>
+                        <a href="" class="product-title">Prita</a>
                         <span class="product-description">
-                          26" Mongoose Dolomite Men's 7-speed, Navy Blue.
+                          Upah minimum daerah sini....
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -371,9 +371,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Xbox One</a>
+                        <a href="" class="product-title">Tarjo</a>
                         <span class="product-description">
-                          Xbox One Console Bundle with Halo Master Chief Collection.
+                          Para buruh ini semakin...
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -403,9 +403,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Samsung TV</a>
+                        <a href="" class="product-title">Silvi</a>
                         <span class="product-description">
-                          Samsung 32" 1080p 60Hz LED Smart HDTV.
+                          Honor Guru Lepas
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -414,9 +414,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Bicycle</a>
+                        <a href="" class="product-title">Budi Karno</a>
                         <span class="product-description">
-                          26" Mongoose Dolomite Men's 7-speed, Navy Blue.
+                          Ruangan Kelas Sekolah sini
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -425,9 +425,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Bicycle</a>
+                        <a href="" class="product-title">Anonymous</a>
                         <span class="product-description">
-                          26" Mongoose Dolomite Men's 7-speed, Navy Blue.
+                          Tolong diperhatikan anak....
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -455,23 +455,23 @@
                   <ul class="products-list product-list-in-box">
                     <li class="item">
                       <div class="product-img">
-                        <img src="dist/img/default-50x50.gif">
+                        <img src="dist/img/user3-128x128.jpg">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Samsung TV</a>
+                        <a href="" class="product-title">Clara Lupita</a>
                         <span class="product-description">
-                          Samsung 32" 1080p 60Hz LED Smart HDTV.
+                          Kepada Yth. Pemerintah Provinsi DKI Jakarta. Jalan di sebelah Untar (JL. Taman S Parman Blok C) jalannya sudah rusak parah dan kalau hujan sudah pasti tergenang. Sudah hampir setahun kondisinya begini dan kian hari makin parah. Mohon bantuannya untuk diperbaiki. Terima kasih.
                         </span>
                       </div>
                     </li><!-- /.item -->
                     <li class="item">
                       <div class="product-img">
-                        <img src="dist/img/default-50x50.gif">
+                        <img src="dist/img/user2-160x160.jpg">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Bicycle</a>
+                        <a href="" class="product-title">Bambang Pamungkis</a>
                         <span class="product-description">
-                          26" Mongoose Dolomite Men's 7-speed, Navy Blue.
+                          Saya ingin melaporkan bahwa kini penjualan satwa liar (dilindungi) tidak hanya terjadi di pinggir jalan saja, namun makin merambah dalam bentuk media sosial yang sangat sulit untuk dideteksi. Mohon responnya. Tks.
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -502,9 +502,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Samsung TV</a>
+                        <a href="" class="product-title">Contoh</a>
                         <span class="product-description">
-                          Samsung 32" 1080p 60Hz LED Smart HDTV.
+                          Ini hanya sebagai contoh...
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -513,9 +513,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Bicycle</a>
+                        <a href="" class="product-title">Contoh</a>
                         <span class="product-description">
-                          26" Mongoose Dolomite Men's 7-speed, Navy Blue.
+                          Ini hanya sebagai contoh...
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -546,9 +546,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Samsung TV</a>
+                        <a href="" class="product-title">Contoh</a>
                         <span class="product-description">
-                          Samsung 32" 1080p 60Hz LED Smart HDTV.
+                          Ini hanya sebagai contoh...
                         </span>
                       </div>
                     </li><!-- /.item -->
@@ -557,9 +557,9 @@
                         <img src="dist/img/default-50x50.gif">
                       </div>
                       <div class="product-info">
-                        <a href="" class="product-title">Bicycle</a>
+                        <a href="" class="product-title">Contoh</a>
                         <span class="product-description">
-                          26" Mongoose Dolomite Men's 7-speed, Navy Blue.
+                          Ini hanya sebagai contoh...
                         </span>
                       </div>
                     </li><!-- /.item -->
