906bd8b33a33b4b0e7246ff8520cffe575376d67
BayuWidia <bayuwidiasantoso@gmail.com>
2016-06-13 16:57:12 -0700

    coba

--- application/app/Http/Controllers/LihatPengaduanController.php
+++ application/app/Http/Controllers/LihatPengaduanController.php
@@ -19,14 +19,15 @@ class LihatPengaduanController extends Controller
       $userid = User::find($idlogin);
 
       $getdatapengaduan = DB::table('pengaduan')
-                      ->join('topik_pengaduan', 'pengaduan.topik_id', '=', 'topik_pengaduan.id')
-                      ->join('master_skpd', 'topik_pengaduan.id_skpd', '=', 'master_skpd.id')
-                      ->join('users', 'users.id', '=', 'pengaduan.warga_id')
-                      ->select('*', 'pengaduan.id', 'users.id as iduser')
-                      ->where('master_skpd.id', $userid->id_skpd)
-                      ->where('flag_mutasi', '0')
-                      ->orderby('pengaduan.created_at', 'desc')
-                      ->paginate(10);
+                    ->join('topik_pengaduan', 'pengaduan.topik_id', '=', 'topik_pengaduan.id')
+                    ->join('master_skpd', 'topik_pengaduan.id_skpd', '=', 'master_skpd.id')
+                    ->join('users', 'users.id', '=', 'pengaduan.warga_id')
+                    ->select('*', 'pengaduan.id', 'users.id as iduser')
+                    ->where('master_skpd.id', $userid->id_skpd)
+                    ->where('flag_tanggap', '0')
+                    ->where('flag_verifikasi', '0')
+                    ->orderby('pengaduan.created_at', 'desc')
+                    ->paginate(10);
                       // dd($getdatapengaduan);
       return view('pages.lihatpengaduan')->with('data', compact('getdatapengaduan'));
     }
