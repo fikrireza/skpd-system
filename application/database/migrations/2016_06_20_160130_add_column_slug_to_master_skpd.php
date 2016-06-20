29de85aabe5b9aa83c3c8c6ac4fe6f5e59b1a6ad
Fikri Reza Alhadi <fikrirezaa@gmail.com>
2016-06-20 16:16:21 +0700

    add slug column into master skpd

---
+++ application/database/migrations/2016_06_20_160130_add_column_slug_to_master_skpd.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnSlugToMasterSkpd extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('master_skpd', function(Blueprint $table){
+        $table->string('slug')->unique()->after('nama_skpd');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
