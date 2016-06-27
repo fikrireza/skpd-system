04d9b14a776ee55eaf733c347794783bf9ff437d
Fikri Reza Alhadi <fikrirezaa@gmail.com>
2016-06-27 09:42:58 +0700

    intervention image plugin

--- application/config/app.php
+++ application/config/app.php
@@ -157,6 +157,9 @@ return [
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
 
+        // Manipulate Images
+        Intervention\Image\ImageServiceProvider::class,
+
     ],
 
     /*
@@ -204,7 +207,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form'		=> Collective\Html\FormFacade::class,
 		    'HTML'		=> Collective\Html\HtmlFacade::class,
-
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
