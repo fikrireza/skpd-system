7df4b80016c55e30b4084a1c2cbdbe634ac164c6
dudyali <dudyali@gmail.com>
2016-07-13 10:37:17 +0700

    remove search box from sidebar admin & user skpd

--- application/resources/views/includes/sidebar.blade.php
+++ application/resources/views/includes/sidebar.blade.php
@@ -25,16 +25,7 @@
       </a>
     </div>
   </div>
-  <!-- search form -->
-  <form action="{{url('pengaduanbytopik')}}" method="get" class="sidebar-form">
-    <div class="input-group">
-      <input type="text" name="q" class="form-control" placeholder="Cari Topik Pengaduan">
-      <span class="input-group-btn">
-        <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
-      </span>
-    </div>
-  </form>
-  <!-- /.search form -->
+  
   <!-- sidebar menu: : style can be found in sidebar.less -->
   <ul class="sidebar-menu">
     <li class="header">NAVIGASI UTAMA</li>
