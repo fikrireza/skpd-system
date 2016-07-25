adaea96e786c18c21bc22ca4429263dfcdc5e43c
BayuWidia <bayuwidiasantoso@gmail.com>
2016-07-25 11:53:32 +0700

    menghapus notif diheader admin

--- application/resources/views/includes/header.blade.php
+++ application/resources/views/includes/header.blade.php
@@ -17,34 +17,6 @@
   </a>
   <div class="navbar-custom-menu">
     <ul class="nav navbar-nav">
-      <!-- Notifications: style can be found in dropdown.less -->
-      <li class="dropdown notifications-menu">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-          <i class="fa fa-bell-o"></i>
-          <span class="label label-warning">2</span>
-        </a>
-        <ul class="dropdown-menu">
-          <li class="header">Anda memiliki 2 notifikasi</li>
-          <li>
-            <!-- inner menu: contains the actual data -->
-            <ul class="menu">
-              <li>
-                <a href="#">
-                  <i class="fa fa-users text-aqua"></i>  warga mendaftar hari ini
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa fa-users text-red"></i>
-
-                  pengaduan belum diproses
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </li>
-
       <!-- User Account: style can be found in dropdown.less -->
       <li class="dropdown user user-menu">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
